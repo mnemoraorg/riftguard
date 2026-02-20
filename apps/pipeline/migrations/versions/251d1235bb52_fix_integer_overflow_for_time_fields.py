0130ffd948dcefcd1b5285dfdff2613e23ac1bd2
Francis Ignacio <hello@francistries.science>
2026-02-20 13:33:27 +0800

    refactor: Simplify type annotations in migration script for clarity

--- apps/pipeline/migrations/versions/251d1235bb52_fix_integer_overflow_for_time_fields.py
+++ apps/pipeline/migrations/versions/251d1235bb52_fix_integer_overflow_for_time_fields.py
@@ -5,17 +5,16 @@ Revises: a42081615b36
 Create Date: 2026-02-20 02:10:58.299151
 
 """
-from typing import Sequence, Union
+from collections.abc import Sequence
 
-from alembic import op
 import sqlalchemy as sa
-
+from alembic import op
 
 # revision identifiers, used by Alembic.
 revision: str = '251d1235bb52'
-down_revision: Union[str, Sequence[str], None] = 'a42081615b36'
-branch_labels: Union[str, Sequence[str], None] = None
-depends_on: Union[str, Sequence[str], None] = None
+down_revision: str | Sequence[str] | None = 'a42081615b36'
+branch_labels: str | Sequence[str] | None = None
+depends_on: str | Sequence[str] | None = None
 
 
 def upgrade() -> None:
